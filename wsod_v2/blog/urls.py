2a78cae9236d7f98d02a97a31160dccbbfcea148
111Helix <poundpoundtp@gmail.com>
2020-07-24 16:50:00 +0700

    added news.html

--- wsod_v2/blog/urls.py
+++ wsod_v2/blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views #The . here is signifying that it's on the current directory
 
 urlpatterns = [
-    path('', views.index, name="blog_index"), #views.home = name_of_file(without.py).name_of_function
-
+    path("", views.index, name="blog_index"), #views.index = name_of_file(without.py).name_of_function
+    path("news/", views.news, name="blog_news")
 ]
