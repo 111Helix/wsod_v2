9516733f12cd958a05e5a4f06c4a2035740b416b
111Helix <poundpoundtp@gmail.com>
2020-07-24 17:49:46 +0700

    changed news format, added new pages

--- wsod_v2/blog/urls.py
+++ wsod_v2/blog/urls.py
@@ -3,5 +3,8 @@ from . import views #The . here is signifying that it's on the current directory
 
 urlpatterns = [
     path("", views.index, name="blog_index"), #views.index = name_of_file(without.py).name_of_function
-    path("news/", views.news, name="blog_news")
+    path("verHistory/", views.verHistory, name="blog_verHistory"),
+    path("bugFix/", views.bugFix, name="blog_bugFix"),
+    path("voteResults/", views.voteResults, name="blog_voteResults"),
+    path("roadMap/", views.roadMap, name="blog_roadMap"),
 ]
--- wsod_v2/blog/views.py
+++ wsod_v2/blog/views.py
@@ -4,5 +4,14 @@ from django.shortcuts import render #This reduces the amount of code needed to r
 def index(request): #This is routes what users will see on the homepage
     return render(request, "blog/index.html") #"blog/indexNonRegistered.html" = name_of_app/template_name_to_render
 
-def news(request):
-    return render(request, "blog/news.html")
+def verHistory(request):
+    return render(request, "blog/verHistory.html")
+
+def bugFix(request):
+    return render(request, "blog/bugFix.html")
+
+def voteResults(request):
+    return render(request, "blog/voteResults.html")
+
+def roadMap(request):
+    return render(request, "blog/roadMap.html")
